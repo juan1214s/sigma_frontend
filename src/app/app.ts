b0c458f33f124b00831e59ceb9ff1f240fbdab0c
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2026-01-03 16:10:12 -0500

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('technical_test_sigma_frontend');
+}
